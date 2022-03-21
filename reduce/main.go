872a703d805da6bd53e680b5180b4b42f2d2e1af
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2022-03-21 21:46:22 +0900

    go fmt

--- reduce/main.go
+++ reduce/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func reduce [T, M any](s []T, f func(M, T) M, initValue M) M {
+func reduce[T, M any](s []T, f func(M, T) M, initValue M) M {
 	acc := initValue
 	for _, v := range s {
 		acc = f(acc, v)
@@ -14,13 +14,13 @@ func reduce [T, M any](s []T, f func(M, T) M, initValue M) M {
 
 func main() {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	sum := reduce(numbers, func(acc, current int) int{
+	sum := reduce(numbers, func(acc, current int) int {
 		return acc + current
 	}, 0)
 	// 55
 	fmt.Println(sum)
 
-	devided := reduce(numbers, func(acc float64, current int) float64{
+	devided := reduce(numbers, func(acc float64, current int) float64 {
 		return acc + float64(current)/10.0
 	}, 0)
 	// 5.5
