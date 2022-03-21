222760af0d9c916d5039b904bd2b3c119cdd78bc
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2022-03-21 21:42:48 +0900

    Implement reduce function

---
+++ reduce/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func reduce [T, M any](s []T, f func(M, T) M, initValue M) M {
+	acc := initValue
+	for _, v := range s {
+		acc = f(acc, v)
+	}
+	return acc
+}
+
+func main() {
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	sum := reduce(numbers, func(acc, current int) int{
+		return acc + current
+	}, 0)
+	// 55
+	fmt.Println(sum)
+
+	devided := reduce(numbers, func(acc float64, current int) float64{
+		return acc + float64(current)/10.0
+	}, 0)
+	// 5.5
+	fmt.Println(devided)
+}
