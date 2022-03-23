600316129a4de0c68620f8f5fc4022849888f129
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2022-03-24 00:14:27 +0900

    Add filter function test

--- filter/filter_test.go
+++ filter/filter_test.go
@@ -6,22 +6,64 @@ import (
 	"testing"
 )
 
-func TestFilter(t *testing.T) {
-	numbers := []int{1, 2, 3, 4, 5}
-	got := filter(numbers, func(i int) bool {
-		return i%2 == 0
-	})
-	want := []int{2, 4}
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("filter got %v want %v\n", got, want)
+func TestFilter_IntSlice(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []int
+		want  []int
+	}{
+		{
+			name:  "filter even numbers",
+			input: []int{1, 2, 3, 4, 5},
+			want:  []int{2, 4},
+		}, {
+			name:  "filter even numbers (empty slice)",
+			input: []int{},
+			want:  []int{},
+		}, {
+			name:  "filter even numbers (no match)",
+			input: []int{1, 3, 5, 7, 9},
+			want:  []int{},
+		},
 	}
 
-	chars := []string{"apple", "banana", "cat", "dog", "gorilla"}
-	got2 := filter(chars, func(str string) bool {
-		return strings.Contains(str, "o")
-	})
-	want2 := []string{"dog", "gorilla"}
-	if !reflect.DeepEqual(got2, want2) {
-		t.Errorf("filter got %v want %v\n", got, want)
+	for _, tt := range tests {
+		got := filter(tt.input, func(i int) bool {
+			return i%2 == 0
+		})
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("filter got %v want %v\n", got, tt.want)
+		}
+	}
+}
+
+func TestFilter_StringSlice(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []string
+		want  []string
+	}{
+		{
+			name:  "filter contains keyword",
+			input: []string{"apple", "banana", "cat", "dog", "gorilla"},
+			want:  []string{"dog", "gorilla"},
+		}, {
+			name:  "filter contains keyword (empty slice)",
+			input: []string{},
+			want:  []string{},
+		}, {
+			name:  "filter contains keyword (no match keyword)",
+			input: []string{"a", "b", "c", "d", "e"},
+			want:  []string{},
+		},
+	}
+
+	for _, tt := range tests {
+		got := filter(tt.input, func(str string) bool {
+			return strings.Contains(str, "o")
+		})
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("filter got %v want %v\n", got, tt.want)
+		}
 	}
 }
