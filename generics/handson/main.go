f1151bc1f7513250552cd16f86f46aeee94c333f
hirasawayuki <yuki.hirasawa1206@gmail.com>
2024-07-15 21:45:02 +0900

    impl apply filter func

--- generics/handson/main.go
+++ generics/handson/main.go
@@ -3,11 +3,42 @@ package main
 import "fmt"
 
 func main() {
+	var sum int
+	ss1 := []int{1, 2, 3, 4, 5}
+
+	ss1 = Filter(ss1, func(n int) bool {
+		return n%2 == 1
+	})
+
+	Apply(ss1, func(n int) {
+		sum += n
+	})
+
+	fmt.Println(sum)
+
 	var ss []string = Map([]int{10, 20}, func(n int) string {
 		return fmt.Sprintf("0x%x", n)
 	})
 
 	fmt.Println(ss)
+}
+
+func Apply(s []int, f func(n int)) {
+	for _, v := range s {
+		f(v)
+	}
+}
+
+func Filter[T any](s []T, f func(n T) bool) []T {
+	var ss []T
+
+	for _, v := range s {
+		if f(v) {
+			ss = append(ss, v)
+		}
+	}
+
+	return ss
 }
 
 func Map[S, T any](s []S, f func(n S) T) []T {
